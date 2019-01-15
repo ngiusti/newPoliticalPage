3b790aabaa6ae8b993dc2be90abdc3629aeac52f
Nick <nick@airtype.com>
2019-01-15 14:06:00 -0800

    changed the port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var StaticServer = require('static-server');
 
 var server = new StaticServer({
     rootPath: "./public/",
-    port: 3000
+    port: process.env.PORT || 3000
 });
 
 server.start(function(){
